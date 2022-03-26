5c5d610c398cac1f8ae11a62e8ee9c38579a9381
Alex Ionitel <oranggsoda@gmail.com>
2022-03-26 17:56:13 -0400

    added license

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from data_loader import *
+import torch
+
+device = torch.device('cudda' if torch.cuda.is_available() else 'cpu')
 
 if __name__ == '__main__':
-    trainset, testset = download_data()
-    
+    print(device)
---
+++ model/helpers.py
@@ -0,0 +1,30 @@
+import torch
+import matplotlib.pyplot as plt
+from data_loader.helpers import imshow
+
+def visualize_model(model, num_images=6, trainloader=None):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
+
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(trainloader):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images//2, 2, images_so_far)
+                ax.axis('off')
+                imshow(inputs.cpu().data[j])
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
