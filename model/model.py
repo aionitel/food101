db3fea37560d7d9be2b5e3b1b17d28f406adc717
Alex Ionitel <oranggsoda@gmail.com>
2022-03-27 01:43:55 -0400

    finished model for now

--- model/model.py
+++ model/model.py
@@ -1,6 +1,12 @@
 import torchvision
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.nn as nn
 
 model = torchvision.models.MobileNetV2(pretrained=True)
 
-num_ftrs = model.fc.in_features
+criterion = nn.CrossEntropyLoss()
 
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
---
+++ train/train.py
@@ -0,0 +1,4 @@
+from model import *
+
+def train_model(model=model, criterion=criterion, optimizer=optimizer, scheduler=lr_scheduler, n_epochs=25):
+  
