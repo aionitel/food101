27fcd4ec0dbf3a9f5e956d6f4298b392097af2d5
Alex Ionitel <oranggsoda@gmail.com>
2022-03-27 19:00:05 -0400

    better train function

--- train.py
+++ train.py
@@ -1,8 +1,12 @@
 from model import *
+from data_loader import *
 import time
 
 # main training function
 def train_model(model=model, criterion=criterion, optimizer=optimizer, scheduler=lr_scheduler, n_epochs=25):
+    trainset, testset = download_data()
+    trainloader, testloader = load_data(trainset, testset)
+
     since = time.time()
 
     best_acc = 0.0
@@ -14,10 +18,33 @@ def train_model(model=model, criterion=criterion, optimizer=optimizer, scheduler
 
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
-            if phase == 'train':
-                model.train()
-            else:
-                model.eval()
-            
             running_loss = 0.0
             running_corrects = 0
+
+            # training epoch
+            if phase == 'train':
+                # set training mode
+                model.train()
+                
+                for inputs, labels in trainloader:
+                    inputs.to(device)
+                    labels.to(device)
+
+                    # zero parameter gradients
+                    optimizer.zero_grad()
+
+                    # forward pass
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
+
+                        # backward pass
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+        
+# root training activation func
+if __name__ == '__main__':
+    # trained model for future use
+    food_model = train_model()
