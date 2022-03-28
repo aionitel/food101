3a8209d62186edd34418bf26ede481e928fa1856
Alex Ionitel <oranggsoda@gmail.com>
2022-03-28 01:14:17 -0400

    now using inception_v3 and adding final linear layer

--- model/model.py
+++ model/model.py
@@ -8,27 +8,23 @@ from torchsummary import summary
 # set device
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# base pytorch model
-model = torchvision.models.densenet121(pretrained=True)
+# base densenet model
+model = torchvision.models.inception_v3(pretrained=True)
 
-print(model.eval)
+num_ftrs = model.fc.in_features
+
+print(num_ftrs)
 
 # freeze all layers
 for param in model.parameters():
     param.requires_grad = False
 
-model.classifer = nn.Sequential(nn.Linear(1024, 512), nn.LeakyReLU(), nn.Linear(512, 256), nn.LeakyReLU(), nn.Linear(256, 101)) # add final trainable linear layers
-
-print(model.eval)
-
-# move model to device (cpu or gpu)
-model = model.to(device)
+# add final trainable linear layers
+model.fc = nn.Linear(num_ftrs, 101) # add final linear layer for 101 classes
 
 # loss function
 criterion = nn.CrossEntropyLoss()
 
-# stochastic gradient descent optimizer
-optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
+optimizer = optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)
 
-# learning rate
-lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+lr_rate = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
